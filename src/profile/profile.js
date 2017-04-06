e9cbeb88b44c727b91160e25eea53ad3c55fd27a
Ryan Swope <rswope@middlebury.edu>
2017-04-05 21:56:37 -0400

    Basic profile selecting functionality, displays basic profile info

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -2,7 +2,7 @@
   profile.js
 
   This provides the implementation for the profile component.
-  
+
 */
 
 
@@ -13,15 +13,60 @@ class Profile extends Component{
   constructor(){
     super();
 
-    console.log(data);
+// Mock sign-in mechanism so we can "choose" which user to show
     this.state = {
       username: '',
+      user: null
     }
   };
 
+  handleUsername(inputEvent){
+    this.setState({username: inputEvent.target.value});
+  }
+
+  handleSignIn(username){
+    for (let profile of data){
+      if (profile.username === username){
+        this.setState({user: username});
+        this.setState({tracks: profile.songs});
+        this.setState({followers: profile.followers.length});
+        this.setState({genre: profile.genre });
+        return;
+      }
+    }
+    alert("This is not a valid user! Please try again.");
+  }
+
   render() {
+    let usernameInput = (<input type="text" value={this.state.username} onChange={(event)=>{this.handleUsername(event)}}/>);
+    let signIn = (<button onClick={()=>this.handleSignIn(this.state.username)}>Sign In</button>);
+
+    if (this.state.user){
+      return(
+        <div>
+          <div>
+          {usernameInput}
+          </div>
+          <div>
+        {signIn}
+          </div>
+          <h1>{this.state.user}</h1>
+          <h2>Followers: {this.state.followers}</h2>
+          <h2>Genre: {this.state.genre}</h2>
+          <h2>Promoted Tracks: {this.state.tracks}</h2>
+
+        </div>
+      );
+    }
+
     return(
       <div>
+        <div>
+        {usernameInput}
+        </div>
+        <div>
+      {signIn}
+        </div>
       </div>
     );
   }

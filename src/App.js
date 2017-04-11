4e35239486ee6e9c549fd50c375d551af2070570
hbeach893 <hannahbeach983@hannahs-mbp-4.middlebury.edu>
2017-04-10 21:09:55 -0400

    Seperated login and profile and pushed their state up to app.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import styled from 'styled-components';
 import MatchPage from './MatchingPage/MatchPage.js';
 import Profile from './profile/profile.js';
 import data from '../public/sounderUsers.json';
-
+import Login from './Components/Login.js';
 
 const CenteredTitle=styled.h1`
   text-align: center;
@@ -23,23 +23,54 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      mode: 'home'
+      mode: 'login'
 
 
     }
   }
+
+  handleSignIn(username){
+    for (let profile of data){
+      if (profile.username === username){
+        this.setState({user: username});
+        this.setState({profilePicture: profile.profilePicture});
+        this.setState({karma: profile.karma});
+        this.setState({tracks: profile.songs});
+        this.setState({followers: profile.followers.length});
+        this.setState({genre: profile.genre });
+        this.setState({profileURL: profile.profileURL});
+        this.setState({mode: 'home'});
+        return;
+      }
+    }
+    alert("This is not a valid user! Please try again.");
+  }
+
+
+  handleLogOut(){
+      this.setState({user: null, username: '',mode:'login'});
+    }
+
+
   render() {
     if(this.state.mode =='home'){
       return (
         <div className="App">
-          <Profile setMode={(article)=>this.setState({mode:'matching'})}/>
-
+          <Profile setLogout={()=>this.handleLogOut()} user={this.state.user} setMode={()=>this.setState({mode:'matching'})}/>
         </div>
       );
-    } else {
+    }
+    if(this.state.mode =='login'){
       return (
         <div className="App">
-          <MatchPage setMode={(article)=>this.setState({mode:'home'})} />
+          <Login setProfile={(username)=>this.handleSignIn(username)} setMode={(article)=>this.setState({mode:'home'})}/>
+        </div>
+      );
+    }
+    else {
+      return (
+        <div className="App">
+          <MatchPage setMode={()=>this.setState({mode:'home'})} />
         </div>
       );
     }
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,1 +1,43 @@
-Login.js
+/*
+  Login.js
+
+  This provides the implementation for the login component.
+
+*/
+import React, { Component } from 'react';
+import data from "../../public/sounderUsers.json";
+
+class Login extends Component{
+  constructor(){
+    super();
+    this.state = {
+      username: '',
+      user: null
+    }
+  };
+
+handleUsername(inputEvent){
+  this.setState({username: inputEvent.target.value});
+}
+
+render() {
+  let usernameInput = (<input type="text" value={this.state.username} onChange={(event)=>{this.handleUsername(event)}}/>);
+  let signIn = (<button onClick={()=>this.props.setProfile(this.state.username)}>Sign In</button>);
+
+  return(
+    <div>
+      <h1>
+      Sounder
+      </h1>
+      <div>
+      {usernameInput}
+      </div>
+      <div>
+    {signIn}
+      </div>
+    </div>
+    );
+
+  }
+}
+export default Login;

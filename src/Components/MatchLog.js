9b47d5c5375b74f3521c934d4a479c6ce5e1c9ba
ben-tindall <tindall.ben@gmail.com>
2017-04-17 22:41:23 -0400

    Styling on matchlog

--- src/Components/MatchLog.js
+++ src/Components/MatchLog.js
@@ -16,20 +16,35 @@ import styled from 'styled-components';
 import Grid from 'react-bootstrap/lib/Grid.js';
 import Row from 'react-bootstrap/lib/Row.js';
 import Col from 'react-bootstrap/lib/Col.js';
+import Panel from 'react-bootstrap/lib/Panel.js';
+import ButtonGroup from 'react-bootstrap/lib/ButtonGroup.js';
+import Button from 'react-bootstrap/lib/Button.js';
+import ListGroup from 'react-bootstrap/lib/ListGroup.js';
+import ListGroupItem from 'react-bootstrap/lib/ListGroupItem.js';
+import '../index.css';
 
-
-
+const CenteredTitle=styled.h1`
+  font-weight: bold;
+  text-align: center;
+  border-bottom: 2px solid #ff4b00;
+`;
 
 function MatchLog(props){
   const matchlog = (props.matchlist).map((user)=>{
     let name = user.username;
-    return (<Row key={name} value={name} onClick={()=>{console.log(user);props.clickMatch(user)}}><Col>{name}</Col></Row>);
+    return (<ListGroupItem key={name} value={name} onClick={()=>{console.log(user);props.clickMatch(user)}}>{name}</ListGroupItem>);
   });
+
   return (
-    <Grid>
-    <Row><Col> <h1> Matches</h1></Col> </Row>
-    <Row><Col> <h4> Click on any Match to their profile!</h4></Col> </Row>
-    {matchlog}
+    <Grid bsStyle="border: 1px solid black">
+      <Row>
+        <Col lg={2}><CenteredTitle>Matches</CenteredTitle></Col>
+      </Row>
+      <Row>
+        <Col lg={2}>
+          <ListGroup>{matchlog}</ListGroup>
+        </Col>
+      </Row>
     </Grid>
   );
 };

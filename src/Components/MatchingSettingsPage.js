84ab3971f3674e47dd0e4fb6ede9a0460e750df8
jyorke <jyorke@middlebury.edu>
2017-04-17 21:33:45 -0400

    added padding to new m'middlerow' row type

--- src/Components/MatchingSettingsPage.js
+++ src/Components/MatchingSettingsPage.js
@@ -9,6 +9,10 @@ import MenuItem from 'react-bootstrap/lib/MenuItem.js'
 import React, { Component } from 'react';
 import data from "../../public/sounderUsers.json";
 import Button from 'react-bootstrap/lib/Button.js';
+import Grid from 'react-bootstrap/lib/Grid.js';
+import Row from 'react-bootstrap/lib/Row.js';
+import Col from 'react-bootstrap/lib/Col.js';
+
 
 
 class MatchingSettings extends Component{
@@ -17,6 +21,7 @@ class MatchingSettings extends Component{
     this.state = {
       min:0,
       max:1000
+
     }
   };
 
@@ -31,9 +36,9 @@ class MatchingSettings extends Component{
   }
 
   render() {
-
     let minFollowers = (<input style={{display: 'inline'}} id="minFollowers" type="text" value={this.state.min} onChange={(event)=> {this.handleMin(event)}} />);
     let maxFollowers = (<input style={{display: 'inline'}} id="maxFollowers" type="text" value={this.state.max} onChange={(event)=> {this.handleMax(event)}} />);
+
     let genre = (<DropdownButton title='Genres' id='genre-dropdown'>
               <MenuItem eventKey= '1'> Alternative Rock </MenuItem>
               <MenuItem eventKey= '2'> Ambient </MenuItem>
@@ -67,22 +72,28 @@ class MatchingSettings extends Component{
               <MenuItem eventKey= '30'> World </MenuItem>
               </DropdownButton>)
       return(
-        <div>
-          <h1>
-          Matching Settings
-          </h1>
+        <Grid>
+          <Row bsClass="topRow">
+            <h1>
+            Matching Settings
+            </h1>
+              <Row bsClass="middleRow">
+                <div id="slider" >
+                  <h5 style={{display: 'inline'}}> Follower Range </h5>
+                  {minFollowers}
+                  <p style={{display: 'inline'}}> to </p>
+                  {maxFollowers}
+                </div>
+              </Row>
+              <Row bsClass="middleRow">
+                <div>
+                  {genre}
+                </div>
+              </Row>
+              <Button value="Back to Home" onClick={()=>this.props.setMode()}>Back To Home</Button>
+          </Row>
+        </Grid>
 
-          <div id="slider" >
-            {minFollowers}
-            <p style={{display: 'inline'}}> to </p>
-            {maxFollowers}
-          </div>
-
-          <div>
-            {genre}
-          </div>
-            <Button value="Back to Home" onClick={()=>this.props.setMode()}>Back To Home</Button>
-        </div>
       );
 
   }

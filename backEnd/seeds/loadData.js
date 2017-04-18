6bb71778e11cad009b657c2ed126003a2e038574
Ryan Swope <rswope@middlebury.edu>
2017-04-17 21:24:32 -0400

    Fixing seeding, changed migration

--- backEnd/seeds/loadData.js
+++ backEnd/seeds/loadData.js
@@ -8,7 +8,7 @@ exports.seed = function(knex, Promise) {
   const userData = data.map((user)=>{
     return {
     username: user.username,
-    profilePicture: user.profilePicture,
+    profilePictureURL: user.profilePicture,
     numFollowers: user.numFollowers,
     karma: user.karma,
     profileURL: user.profileURL,
@@ -18,31 +18,23 @@ exports.seed = function(knex, Promise) {
   };
 });
 
+  const matchData = data.map((user)=>{
+    for (follower of user.followers) {
+      return {
+        id: user.id,
+        follower: follower
+      };
+    };
+  });
+
   // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
       // Inserts seed entries
       return knex('users').insert(userData);
     });
-};
-
-
-exports.seed = function(knew, Promise) {
-  const matchData = data.map((user)=>{
-    for (follower of user.followers) {
-      return {
-        id: user.id,
-        follower: follower
-      }
-    }
-  });
-
-  return knex('matches').del()
     .then(function () {
       // Inserts seed entries
       return knex('matches').insert(matchData);
-    });
+    })
 };
-
-//table name refers to one singular table name
-//returns a promise, way to specify asynchronous behavior that won't fire without .then

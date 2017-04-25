f43936ffe0c04c6f4daa38e25af1b0341f107e91
Geoffrey Ziobro <gziobro20@gmail.com>
2017-04-25 11:06:58 -0400

    Working embedded song from user info

--- frontEnd/src/Components/EmbedSong.js
+++ frontEnd/src/Components/EmbedSong.js
@@ -12,13 +12,13 @@ import React from 'react';
 
 function EmbedSong(props){
   console.log(props);
-  let songString = props.songURL.split('src=');
-  let songNotFinal = songString[1].split(">");
+  let songString = props.songURL.split('src="');
+  let songNotFinal = songString[1].split('"');
   let songFinal = songNotFinal[0];
   console.log(songNotFinal);
   console.log(songFinal);
   let isong = (<iframe width="100%" height="150" scrolling="no" frameBorder="no"
-  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/248839502&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true" ></iframe>);
+  src={songFinal} ></iframe>);
   return(
     <div>
     {isong}
